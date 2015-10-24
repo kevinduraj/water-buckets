e249f70ee72bb15ee238753937e9248d3894f0fd
Kevin Duraj <kduraj@gmail.com>
2015-10-24 09:09:41 -0700

    Keep empty bucket A and move from B to A until B=4

--- src/main/scala/kduraj/Application.scala
+++ src/main/scala/kduraj/Application.scala
@@ -2,49 +2,54 @@ package kduraj
 
 object Application extends App {
 
-  //println("\nWater Buckets - Fill Right to Left")
-  //println("----------------------------------")
-  //right2Left()
+  println("\nWater Buckets - Fill Right to Left")
+  println("----------------------------------")
+  right2Left()
   println("\nWater Buckets - Fill Left to Right")
   println("----------------------------------")
   left2Right()
 
-
-  def left2Right(): Unit = {
-
-    val b = new Buckets()
-
-    /*0*/ b.printContent()
-
-    /*1*/ b.fullA().printContent()
-    /*2*/ b.fillA2B().printContent()
-
-    /*3*/ b.fullA().printContent()
-    /*4*/ b.fillA2B().printContent()
-
-    /*5*/ b.emptyB().printContent()
-    /*6*/ b.fillA2B().printContent()
-
-    /*7*/ b.fullA().printContent()
-    /*8*/ b.fillA2B().printContent()
-
-  }
 
 
   def right2Left(): Unit = {
 
     val buckets = new Buckets()
 
-    /*0*/ buckets.printContent()
+    buckets.printContent()
 
-    /*1*/ buckets.fullB().printContent()
-    /*2*/ buckets.fillB2A().printContent()
+    buckets.fullB().printContent()
+    buckets.fillB2A().printContent()
 
-    /*3*/ buckets.emptyA().printContent()
-    /*5*/ buckets.fillB2A().printContent()
+    // Keep empty bucket A and move from B to A until B=4
+    do {
+      buckets.emptyA().printContent()
+      buckets.fillB2A().printContent()
+      buckets.fullB().printContent()
+      buckets.fillB2A().printContent()
 
-    /*6*/ buckets.fullB().printContent()
-    /*7*/ buckets.fillB2A().printContent()
+    } while (buckets.B != 4)
+  }
+
+  def left2Right(): Unit = {
+
+    val b = new Buckets()
+
+    b.printContent()
+
+    // Keep filling bucket A and move to B until A=1
+    do {
+      b.fullA().printContent()
+      b.fillA2B().printContent()
+    } while (b.A != 1)
+
+    // Keep empty bucket B and move from A to B until B=4
+    do {
+      b.emptyB().printContent()
+      b.fillA2B().printContent()
+      b.fullA().printContent()
+      b.fillA2B().printContent()
+
+    } while (b.B != 4)
 
   }
 
