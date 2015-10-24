c8ea3462e8a1a46abde28528004163e23a31af9b
Kevin Duraj <kduraj@gmail.com>
2015-10-24 13:37:01 -0700

    docs

--- src/main/scala/kduraj/Application.scala
+++ src/main/scala/kduraj/Application.scala
@@ -1,19 +1,36 @@
 package kduraj
 
+/**
+ * Algorithm for filling water into different size of buckets
+ * until one bucket contains target amount of water
+ * Written by: Kevin Duraj
+ * October 24, 2015
+ */
+
 object Application extends App {
 
-  val maxA = 3
-  val maxB = 5
-  val target = 4
+  println("Right to Left A=3,B=5,Target=4")
+  println("------------------------------------")
+  val bucket1 = new Buckets(3, 5, 4)
+  bucket1.right2Left()
+  println
 
-  println("\nWater Buckets - Fill Right to Left")
-  println("----------------------------------")
-  val b1 = new Buckets(maxA, maxB, target)
-  b1.right2Left()
+  println("Left to Right A=3,B=5,Target=4")
+  println("------------------------------------")
+  val bucket2 = new Buckets(3, 5, 4)
+  bucket2.right2Left()
+  println
 
-  println("\nWater Buckets - Fill Left to Right")
-  println("----------------------------------")
-  val b2 = new Buckets(maxA, maxB, target)
-  b2.left2Right()
+  println("Right to Left A=9,B=12,Target=6")
+  println("------------------------------------")
+  val bucket3 = new Buckets(9, 12, 6)
+  bucket3.right2Left()
+  println
+
+  println("Left to Right A=9,B=12,Target=6")
+  println("------------------------------------")
+  val bucket = new Buckets(9, 12, 6)
+  bucket.right2Left()
+  println
 
 }
